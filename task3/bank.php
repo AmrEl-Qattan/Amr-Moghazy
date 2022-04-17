7e8ebecc665c06055387f5d9357fa4c8de4ed3e3
AmrEl-Qattan <amrelqattan95@gmail.com>
2022-04-17 03:45:11 +0200

    task4

--- task3/bank.php
+++ task3/bank.php
@@ -1,22 +1,19 @@
 <?php
-if ($_POST){
-    $name = $_POST['username'];
-    $loan = $_POST['loan'];
-    $years = $_POST['years'];
-    $months = $years * 12;
+if ($_POST) {
+  $name = $_POST['username'];
+  $loan = $_POST['loan'];
+  $years = $_POST['years'];
+  $months = $years * 12;
 
-    if ($years <= 3){
-        $rate = 0.1;
-        
-        
-    } else {
-        $rate = 0.15;
-        
-
-    }$Interstrate = ($loan * $rate) * $years;
-    $Interstloan = $loan + $Interstrate;
-    $monthly = $Interstloan / $months;
-    $message =  (" '<table class='table table-striped col-8 offset-2'>
+  if ($years <= 3) {
+    $rate = 0.1;
+  } else {
+    $rate = 0.15;
+  }
+  $Interstrate = ($loan * $rate) * $years;
+  $Interstloan = $loan + $Interstrate;
+  $monthly = $Interstloan / $months;
+  $message =  (" '<table class='table table-striped col-8 offset-2'>
     <thead>
       <tr>
         <th class='text-primary'> User name</th>
@@ -35,8 +32,8 @@ if ($_POST){
     </tbody>
   </table>';");
 }
-    
-  
+
+
 
 
 ?>
@@ -45,47 +42,48 @@ if ($_POST){
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Bank</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
+<head>
+  <title>Bank</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
   <div class="container">
-        <div class="row">
-            <div class="col-12 text-center mt-5">
-                <h4 class="text-primary">Bank</h4>
-            </div>
-            <div class="col-4 offset-4 mt-5">
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label for="username" class="text-primary">User Name</label>
-                        <input type="username" name="username" id="username" class="form-control" placeholder=" " aria-describedby="helpId" value=<?= isset($_POST['username']) ? $_POST['username'] : '' ?>>
-                        <label for="text" class="text-primary">Loan amount</label>
-                        <input type="number" name="loan" id="loan" class="form-control" placeholder=" " aria-describedby="helpId" value=<?= isset($_POST['loan']) ? $_POST['loan'] : '' ?>>
-                        <label for="Number" class="text-primary">Loan years</label>
-                        <input type="number" name="years" id="years" class="form-control" placeholder=" " aria-describedby="helpId" value=<?= isset($_POST['years']) ? $_POST['years'] : '' ?>>
-                    </div>
-                    <button class="btn btn-primary form-control text-white">Calculate</button>
-                </form><br>
-                
-            </div>
-            <?php echo $message ?? ""; ?><br>
+    <div class="row">
+      <div class="col-12 text-center mt-5">
+        <h4 class="text-primary">Bank</h4>
+      </div>
+      <div class="col-4 offset-4 mt-5">
+        <form action="" method="post">
+          <div class="form-group">
+            <label for="username" class="text-primary">User Name</label>
+            <input type="username" name="username" id="username" class="form-control" placeholder=" " aria-describedby="helpId" value=<?= isset($_POST['username']) ? $_POST['username'] : '' ?>>
+            <label for="text" class="text-primary">Loan amount</label>
+            <input type="number" name="loan" id="loan" class="form-control" placeholder=" " aria-describedby="helpId" value=<?= isset($_POST['loan']) ? $_POST['loan'] : '' ?>>
+            <label for="Number" class="text-primary">Loan years</label>
+            <input type="number" name="years" id="years" class="form-control" placeholder=" " aria-describedby="helpId" value=<?= isset($_POST['years']) ? $_POST['years'] : '' ?>>
+          </div>
+          <button class="btn btn-primary form-control text-white">Calculate</button>
+        </form><br>
 
-        </div>
+      </div>
+      <?php echo $message ?? ""; ?><br>
+
     </div>
-    
+  </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
-
-  
